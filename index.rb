5472343adeb5f5f5f8e813951774d31290a796b5
Isaac Tavares Caldas Gonçalves <50925182+IsaacCaldas@users.noreply.github.com>
2024-08-22 00:44:49 -0300

    Create index.rb

---
+++ index.rb
@@ -0,0 +1,12 @@
+print "Digite seu nome: "
+name = gets.chomp
+
+print "Digite seu sobrenome: "
+surname = gets.chomp
+
+print "Informe sua idade: "
+age = gets.chomp
+
+fullname = "#{name} #{surname}"
+
+puts "Você se chama #{fullname} e tem #{age} anos."
